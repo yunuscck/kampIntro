e73cef409c398aa7b55b3615e4f753479ec6c236
Emre Cicek <ExtEmreCi@d-teknoloji.com.tr>
2022-08-22 00:16:29 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Yunus";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.kursAdi = "C#";
+            kurs1.Egitmeni = "Yunus Çiçek";
+            kurs1.IzlemeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.kursAdi = "Java";
+            kurs2.Egitmeni = "Emre Çiçek";
+            kurs2.IzlemeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.kursAdi = "Pyhton";
+            kurs3.Egitmeni = "Enes Yıldırım";
+            kurs3.IzlemeOrani = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.kursAdi = "C++";
+            kurs4.Egitmeni = "Kadirhan Harput";
+            kurs4.IzlemeOrani = 100;
+
+            
+            //Console.WriteLine (kurs1.kursAdi + " - " + kurs1.Egitmeni);
+
+            //Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+
+            //foreach (Kurs kurs in kurslar)
+            //{
+            //    Console.WriteLine(kurs.kursAdi + " : " + kurs.Egitmeni);
+
+            //}
+
+
+
+            
+
+
+
+
+
+
+
+        // ;  <  >
+    }
+
+    }
+
+    class Kurs
+    {
+        public string  kursAdi { get; set; }
+        public string  Egitmeni { get; set; }
+        public int  IzlemeOrani { get; set; }
+
+
+
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "java";
+            string kurs4 = "Pyhton";
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", 
+                "Programlamaya başlangıç için temel kurs", "java", "Pyhton", "C++" };
+
+
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("----FOR BİTTİ----");
+            
+            foreach (string  kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+
+
+
+            Console.WriteLine("sayfa sonu - footer");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // ;  <  >
+
+
+        }
+    }
+}
